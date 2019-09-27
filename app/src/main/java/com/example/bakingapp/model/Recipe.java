eb7ffde31e43e25715f488f0655e67ddddf4268d
Mohamed Samy <mamo.sha3ar@gmail.com>
2019-09-27 22:24:31 +0200

    added recipe and step models

---
+++ app/src/main/java/com/example/bakingapp/model/Recipe.java
@@ -0,0 +1,119 @@
+package com.example.bakingapp.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Recipe {
+    @SerializedName("id")
+    @Expose
+    private long id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("ingredients")
+    @Expose
+    private List<Ingredient> ingredients = new ArrayList<Ingredient>();
+    @SerializedName("steps")
+    @Expose
+    private List<Step> steps = new ArrayList<Step>();
+    @SerializedName("servings")
+    @Expose
+    private long servings;
+    @SerializedName("image")
+    @Expose
+    private String image;
+
+    public Recipe() {
+    }
+
+    public Recipe(long id, String name, List<Ingredient> ingredients, List<Step> steps, long servings, String image) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.steps = steps;
+        this.servings = servings;
+        this.image = image;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Recipe withId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Recipe withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Recipe withIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+        return this;
+    }
+
+    public List<Step> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<Step> steps) {
+        this.steps = steps;
+    }
+
+    public Recipe withSteps(List<Step> steps) {
+        this.steps = steps;
+        return this;
+    }
+
+    public long getServings() {
+        return servings;
+    }
+
+    public void setServings(long servings) {
+        this.servings = servings;
+    }
+
+    public Recipe withServings(long servings) {
+        this.servings = servings;
+        return this;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Recipe withImage(String image) {
+        this.image = image;
+        return this;
+    }
+}
---
+++ app/src/main/java/com/example/bakingapp/model/Step.java
@@ -0,0 +1,99 @@
+package com.example.bakingapp.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Step {
+    @SerializedName("id")
+    @Expose
+    private long id;
+    @SerializedName("shortDescription")
+    @Expose
+    private String shortDescription;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("videoURL")
+    @Expose
+    private String videoURL;
+    @SerializedName("thumbnailURL")
+    @Expose
+    private String thumbnailURL;
+
+    public Step() {
+    }
+
+    public Step(long id, String shortDescription, String description, String videoURL, String thumbnailURL) {
+        super();
+        this.id = id;
+        this.shortDescription = shortDescription;
+        this.description = description;
+        this.videoURL = videoURL;
+        this.thumbnailURL = thumbnailURL;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Step withId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public Step withShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Step withDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public String getVideoURL() {
+        return videoURL;
+    }
+
+    public void setVideoURL(String videoURL) {
+        this.videoURL = videoURL;
+    }
+
+    public Step withVideoURL(String videoURL) {
+        this.videoURL = videoURL;
+        return this;
+    }
+
+    public String getThumbnailURL() {
+        return thumbnailURL;
+    }
+
+    public void setThumbnailURL(String thumbnailURL) {
+        this.thumbnailURL = thumbnailURL;
+    }
+
+    public Step withThumbnailURL(String thumbnailURL) {
+        this.thumbnailURL = thumbnailURL;
+        return this;
+    }
+}
