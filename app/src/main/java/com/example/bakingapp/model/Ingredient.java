2370e669783f2f260df6110e16010363514de100
Mohamed Samy <mamo.sha3ar@gmail.com>
2019-09-27 22:18:55 +0200

    added ingredient class

---
+++ app/src/main/java/com/example/bakingapp/model/Ingredient.java
@@ -0,0 +1,65 @@
+package com.example.bakingapp.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Ingredient {
+    @SerializedName("quantity")
+    @Expose
+    private long quantity;
+    @SerializedName("measure")
+    @Expose
+    private String measure;
+    @SerializedName("ingredient")
+    @Expose
+    private String ingredient;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(long quantity, String measure, String ingredient) {
+        super();
+        this.quantity = quantity;
+        this.measure = measure;
+        this.ingredient = ingredient;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(long quantity) {
+        this.quantity = quantity;
+    }
+
+    public Ingredient withQuantity(long quantity) {
+        this.quantity = quantity;
+        return this;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public Ingredient withMeasure(String measure) {
+        this.measure = measure;
+        return this;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Ingredient withIngredient(String ingredient) {
+        this.ingredient = ingredient;
+        return this;
+    }
+}
